6f6bb1fc0c22a64d7c837218f594ffe3a716cdfb
fxss <723107234@qq.com>
2022-04-07 10:02:55 +0800

    feat: 测试 $subscribe 、$onAction

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,13 @@
   <h1>Hello Vue 3 + TypeScript + Vite</h1>
   <div>doubleCount: {{ counter.doubleCount }}</div>
   <div>doubleCountPlusOne: {{ counter.doubleCountPlusOne }}</div>
-  <button @click="counter.increment">count: {{ counter.count }}</button>
+  <button @click="counter.$patch({ count: counter.count + 1 })">count: {{ counter.count }}</button>
   <button @click="counter.$reset()">reset</button>
   <div>otherCounter.countSum: {{ otherCounter.countSum }}</div>
   <button @click="otherCounter.increment">count: {{ otherCounter.count }}</button>
+  <button @click="otherCounterDispose">otherCounterDispose</button>
+  <div>counter.countOnAction</div>
+  <button @click="countOnActionEvent">countOnAction</button>
 </template>
 
 <script setup lang="ts">
@@ -15,7 +18,56 @@
 import { useCounterStore, useOtherCounterStore } from './store'
 
 const counter = useCounterStore()
+function countOnActionEvent() {
+  counter.countOnAction(10)
+}
+counter.$subscribe((mutation, state) => {
+  // // import { MutationType } from 'pinia'
+  // mutation.type // 'direct' | 'patch object' | 'patch function'
+  // // same as cartStore.$id
+  // mutation.storeId // 'cart'
+  // // only available with mutation.type === 'patch object'
+  // mutation.payload // patch object passed to cartStore.$patch()
+  console.log(mutation)
+})
+const unsubscribe = counter.$onAction(
+  ({
+    name, // name of the action
+    store, // store instance, same as `someStore`
+    args, // array of parameters passed to the action
+    after, // hook after the action returns or resolves
+    onError, // hook if the action throws or rejects
+  }) => {
+    // a shared variable for this specific action call
+    const startTime = Date.now()
+    // this will trigger before an action on `store` is executed
+    console.log(`Start "${name}" with params [${args.join(', ')}].`)
+
+    // this will trigger if the action succeeds and after it has fully run.
+    // it waits for any returned promised
+    after((result) => {
+      console.log(
+        `Finished "${name}" after ${
+          Date.now() - startTime
+        }ms.\nResult: ${result}.`
+      )
+    })
+
+    // this will trigger if the action throws or returns a promise that rejects
+    onError((error) => {
+      console.warn(
+        `Failed "${name}" after ${Date.now() - startTime}ms.\nError: ${error}.`
+      )
+    })
+  }
+)
+// unsubscribe()
+
 const otherCounter = useOtherCounterStore()
+
+function otherCounterDispose() {
+  otherCounter.$dispose()
+}
 </script>
 
 <style>
--- src/store/counter.ts
+++ src/store/counter.ts
@@ -15,6 +15,9 @@ export const useCounterStore = defineStore('counter', {
   actions: {
     increment() {
       this.count++
+    },
+    countOnAction(val: number) {
+      this.count += val
     }
   }
 })
